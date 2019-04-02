2c0e5a64ca055a1dba65a8263bd2cbd56d7987de
Kamil Rojek <Kamil_Rojek@epam.com>
2019-04-02 11:52:24 +0200

    almost done ex 7

--- src/main/java/com/rojek/kamil/fluentconditionals/FluentConditionals.java
+++ src/main/java/com/rojek/kamil/fluentconditionals/FluentConditionals.java
@@ -1,6 +1,7 @@
 package com.rojek.kamil.fluentconditionals;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
@@ -78,6 +79,22 @@ class FluentConditionals {
             return new ElseReturn<>(condition, t);
         }
 
+//        <R> ThenReturR<R, T> thenReturn(Supplier<T> t) {
+//            return new ThenReturR<>( condition, t.get());
+//        }
+
+        <R> ThenReturR<T, R> thenReturn(Supplier<R> r) {
+            return new ThenReturR<>(t, r.get(), condition);
+        }
+
+        <R> ThenReturR<T, R> thenReturn(Function<T, R> function) {
+            return new ThenReturR<>(t, function.apply(t),condition);
+        }
+
+        ElseReturn<T> thenReturn(T t) {
+            return new ElseReturn<>(condition, t);
+        }
+
         void orElse(Runnable t) {
             if(!condition)
                 t.run();
@@ -107,6 +124,57 @@ class FluentConditionals {
                 throw e;
             return t;
         }
+    }
+
+    static class ThenReturR<T, R>{
+        private T t;
+        private R r;
+        private boolean condition;
+
+//        ThenReturR(boolean condition, T t) {
+//            this.condition = condition;
+//            this.t = t;
+//            //this.r = r;
+//        }
+
+//        ThenReturR(R r, boolean condition) {
+//            this.condition = condition;
+//            this.r = r;
+//            //this.r = r;
+//        }
+
+
+        ThenReturR(T t, R r, boolean condition) {
+            this.t = t;
+            this.r = r;
+            this.condition = condition;
+        }
+
+        void orElse(Runnable r) {
+            if(!condition)
+                r.run();
+        }
+
+        void orElse(Consumer<R> consumer) {
+            if(!condition)
+                consumer.accept(r);
+        }
+
+        R orElse(Supplier<R> r) {
+            return orElse(r.get());
+        }
+
+        R orElse(Function<T, R> function) {
+            return orElse(function.apply(t));
+        }
+
+        R orElse(R r) {
+            if (condition)
+                return this.r;
+            return r;
+        }
+
+
     }
 
 
